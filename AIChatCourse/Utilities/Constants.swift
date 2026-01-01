00a8686e1bc1ceee1400428949600f410149ccc5
Dima Kryzhanovsky <dimakryzha@gmail.com>
2026-01-01 22:15:06 +0200

    [Feature] Add WelcomeView UI.

---
+++ AIChatCourse/Utilities/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  AIChatCourse
+//
+//  Created by Dmitro Kryzhanovsky on 01.01.2026.
+//
+
+import Foundation
+
+struct Constants {
+    static let randomImage = "https://picsum.photos/600/600"
+    static let privacyPolicyURL = "https://www.apple.com"
+    static let termosOfServiceURL = "https://www.apple.com"
+}
