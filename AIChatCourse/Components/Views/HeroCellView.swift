451839db0710588c23f240299e8a3d188c2e4905
Dima Kryzhanovsky <dimakryzha@gmail.com>
2026-01-02 00:19:49 +0200

    [Feature] HeroCellView.

---
+++ AIChatCourse/Components/Views/HeroCellView.swift
@@ -0,0 +1,78 @@
+//
+//  HeroCellView.swift
+//  AIChatCourse
+//
+//  Created by Dmitro Kryzhanovsky on 01.01.2026.
+//
+
+import SwiftUI
+
+struct HeroCellView: View {
+    
+    var title: String? = "This is some title"
+    var subtitle: String? = "This is some subtitle"
+    var imageName: String? = Constants.randomImage
+    
+    var body: some View {
+        ZStack {
+            if let imageName {
+                ImageLoaderView(urlString: imageName)
+            } else {
+                Rectangle()
+                    .fill(.accent)
+            }
+        }
+        .overlay(alignment: .bottomLeading, content: {
+            VStack(alignment: .leading, spacing: 4) {
+                if let title {
+                    Text(title)
+                        .font(.headline)
+                }
+                
+                if let subtitle {
+                    Text(subtitle)
+                        .font(.subheadline)
+                }
+            }
+            .foregroundStyle(.white)
+            .padding(16)
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .background(
+                LinearGradient(colors: [
+                    Color.black.opacity(0.0),
+                    Color.black.opacity(0.3),
+                    Color.black.opacity(0.4)
+                ], startPoint: .top, endPoint: .bottom)
+            )
+        })
+        .cornerRadius(16)
+    }
+}
+
+// Cool
+#Preview {
+    ScrollView {
+        HeroCellView()
+            .frame(width: 300, height: 200)
+        
+        HeroCellView()
+            .frame(width: 300, height: 400)
+        
+        HeroCellView()
+            .frame(width: 200, height: 400)
+        
+        HeroCellView(imageName: nil)
+            .frame(width: 300, height: 200)
+        
+        HeroCellView(title: nil)
+            .frame(width: 300, height: 200)
+        
+        HeroCellView(subtitle: nil)
+            .frame(width: 300, height: 200)
+        
+        HeroCellView(title: nil, subtitle: nil, imageName: nil)
+            .frame(width: 300, height: 200)
+        
+    }
+    .scrollIndicators(.hidden)
+}
